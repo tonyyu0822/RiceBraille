56f6feca4014c69381e1bd821c599c5bc49b7aca
Michael Tang <mjt6@rice.edu>
2020-12-22 15:52:39 -0600

    added comments

--- scan.py
+++ scan.py
@@ -7,12 +7,9 @@ from typing import Any
 
 from pyimagesearch.transform import four_point_transform
 from skimage.filters import threshold_local
-import ar_markers as ar
 import cv2
 import imutils
 import numpy as np
-
-import pyzbar.pyzbar as pyzbar
 
 screenCnt = []
 
@@ -22,28 +19,31 @@ def click(event, x, y, flag, image):
         coords = [[x, y]]
         screenCnt.append(coords)
 
-def transform_image(image, paper_dims=(825, 1100), output_image="scannedImage.jpg", black_background=False, automatic=True):
+# NOTE: no idea what paper_dims is for
+def transform_image(image, paper_dims=(825, 1100), output_image="scannedImage.jpg", black_background=True, automatic=True):
     """
     :param image: image frame
     :param paper_dims: dimensions of paper (in pixels) to scale scanned image to
     :param output_image: name of file to write new image to
-    :param black_background: if True, background will be cast to black before performing page calibration
+    :param black_background: if True, will perform page calibration by casting background to black 
+    :param automatic: if True, will perform page calibration automatically
     :return: returns transformation matrix
     """
-
     global screenCnt
 
-    # preserve original image for later use
+    # preserve original image
     orig = image.copy()
     
     if automatic:
         # page calibration filtering method depends on if background needs to be cast to black
         if black_background:
             # mask everything from neon green to black: helps distinguish page from background
+            # first, turn page to black and background to white
             lower = np.array([30, 50, 50])
             upper = np.array([50, 255, 255])
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, lower, upper)
+            # then, flip color so background is black and page is white
             mask[mask > 0] = 1
             new_mask = np.subtract(np.ones(np.shape(mask)), mask)
             new_mask = new_mask.astype('uint8')
@@ -54,16 +54,14 @@ def transform_image(image, paper_dims=(825, 1100), output_image="scannedImage.jp
             blurred = cv2.GaussianBlur(gray, (5, 5), 0)
             edged = cv2.Canny(blurred, 75, 200)
 
-        # otherwise, use double-blur method (when this method works, it tags corners more accurately, but doesn't always work)
+        # otherwise, use double-blur method (when this method works, it seems to tags corners more accurately, but doesn't always work)
         else:
             # convert the image to grayscale, blur it, and find edges
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             # double blur ensures that braille dots are not picked up by canny filter
             blurred = cv2.GaussianBlur(gray, (5, 5), 0)
             blurred = cv2.GaussianBlur(blurred, (5, 5), 0)
-            show_image(blurred)
             edged = cv2.Canny(blurred, 5, 200, True)
-            show_image(edged)
 
         # find the contours in the edged image, keeping only the largest ones, and initialize the screen contour 
         cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -84,7 +82,7 @@ def transform_image(image, paper_dims=(825, 1100), output_image="scannedImage.jp
 
                 # to verify if we detected correct points
                 red = (0, 0, 255)
-                colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 255), (255, 255, 255), (255, 255, 255)]
+                # colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 255), (255, 255, 255), (255, 255, 255)]
                 for i in range(4):
                     with_corners = cv2.circle(orig, (approx[i][0][0], approx[i][0][1]), radius=5, color=red, thickness=-1)
                 show_image(with_corners)
@@ -114,6 +112,7 @@ def transform_image(image, paper_dims=(825, 1100), output_image="scannedImage.jp
     # apply the four point transform to obtain a top-down
     # view of the original image
     M, warped, dims = four_point_transform(orig, screenCnt.reshape(4, 2))
+    show_image(warped)
     #find_markers(warped)
     # convert the warped image to grayscale, then threshold it
     # to give it that 'black and white' paper effect
@@ -172,6 +171,7 @@ def get_transform_video(video_path, desired_dimensions=(11.5, 11.0), black_backg
     for i in range(75, 25, -5):
         cap.set(cv2.CAP_PROP_POS_FRAMES, video_length - i)
         ret, frame = cap.read()
+        transform_image(frame, automatic=False)
         m, im_dims = transform_image(frame, black_background=black_background)
         if m is not None:
             break
@@ -183,8 +183,7 @@ def get_transform_video(video_path, desired_dimensions=(11.5, 11.0), black_backg
     return TransformMetadata(m, im_dims, desired_dimensions)
 
 if __name__ == '__main__':
-    ############# FOR TESTING
-    video_path = "./test_images/test_1.mp4"
+    video_path = "./test_images/test_0.mp4"
     get_transform_video(video_path)
 
 #transform_metadata = get_transform_video("test_images/test.mp4")
